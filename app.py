80ab85050a473656d22da3ed2a28509a79a586fd
kenyangbali <bayusat008@gmail.com>
2025-10-24 12:01:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import os
 from cerebras.cloud.sdk import Cerebras
 
 # --- Konfigurasi ---
-DEFAULT_MODAL_URL = "https://forreplit0089--example-comfyui-ui.modal.run/"
+DEFAULT_MODAL_URL = "https://oktetod--example-comfyui-ui.modal.run/"
 DEFAULT_NEGATIVE_PROMPT = "(static image:1.5), (still frame:1.4), (motionless:1.3), motion blur, blurry, low quality, bad quality, worst quality, jpeg artifacts, compression, watermark, text, signature, username, error, deformed, mutilated, extra limbs, bad anatomy, ugly, (fused fingers:1.2), (too many fingers:1.2), pixelated, low resolution"
 
 # --- WORKFLOW TEMPLATE ---
